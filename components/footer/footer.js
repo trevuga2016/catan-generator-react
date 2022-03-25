770e134b7bc17f9520b1d8e705174bcb0a27c01e
TXR3WP9 <trevor_richardson@homedepot.com>
2022-03-25 18:19:19 -0400

    adding footer back, reverting some changes

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,17 +1,15 @@
-import { AppBar, Grid, Typography } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import styles from './footer.module.scss';
 
 export const Footer = () => {
     return(
-      <AppBar className={styles["footer"]}>
-        <Grid container className={styles["footer__grid"]}>
+        <Grid container className={styles["footer"]}>
           <Grid item>
             <Typography variant="body2" className={styles["footer__color"]}>
               2022 &copy; Trevor Richardson
             </Typography>
           </Grid>
         </Grid>
-      </AppBar>
     );
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import '../styles/globals.scss';
+import { Footer } from "../components/footer/footer";
 
 function CatanGenerator({ Component, pageProps }) {
   return(
     <>
       <Component {...pageProps} />
+        <Footer />
     </>
   );
 }
