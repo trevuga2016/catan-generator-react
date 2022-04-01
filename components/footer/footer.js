2ec2cc6f2afdef54bc9711eafa8336d392fcc78d
TXR3WP9 <trevor_richardson@homedepot.com>
2022-04-01 15:48:10 -0400

    Updating footer component; legal update in readme

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,12 +1,10 @@
-import { Grid, Typography } from '@mui/material';
+import { Grid } from '@mui/material';
 import styles from './footer.module.scss';
 
 export const Footer = () => {
     return(
         <Grid container className={styles["footer"]}>
-            <Typography variant="body2" className={styles["footer__color"]}>
               2022 &copy; Trevor Richardson
-            </Typography>
         </Grid>
     );
 }
