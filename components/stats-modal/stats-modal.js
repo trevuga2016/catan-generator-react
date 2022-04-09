01ed8a829bb51f34fdb6144446d6e8ed64a02a5c
TXR3WP9 <trevor_richardson@homedepot.com>
2022-04-09 12:09:45 -0400

    Handling cleared board stats

--- components/stats-modal/stats-modal.js
+++ components/stats-modal/stats-modal.js
@@ -4,28 +4,16 @@ import InsertPhotoOutlinedIcon from '@mui/icons-material/InsertPhotoOutlined';
 
 export const StatsModal = ({ open, onClose, stats }) => {
 
-    const maResource = stats?.length > 0 ? stats?.[0]?.resource : '--';
-    const laResource = stats?.length > 0 ? stats?.[stats?.length - 1]?.resource : '--';
+    const maResource = stats?.[0]?.probability !== '0.000' ? stats?.[0]?.resource : undefined;
+    const laResource = stats?.[stats?.length - 1]?.probability !== '0.000' ? stats?.[stats?.length - 1]?.resource : undefined;
 
     return(
         <Modal open={open} onClose={onClose}>
             <Box className={styles["modal"]}>
                 <Grid container direction="column" className={styles["modal__container"]} p={5}>
                     <Typography variant="h4">Board Statistics</Typography>
-                    <Typography variant="h6">Frequency of Resource per Turn</Typography>
+                    <Typography variant="h6">Probability of Collecting Resource per Turn</Typography>
                     <Grid container direction="column" className={styles["modal__container__stats"]}>
-                    { stats?.length === 0 &&
-                        <Grid item className={styles["modal__container__stats"]}>
-                            <ListItem>
-                                <ListItemAvatar>
-                                    <Avatar><InsertPhotoOutlinedIcon /></Avatar>
-                                </ListItemAvatar>
-                                <ListItemText>
-                                    <Typography variant="h6">--</Typography>
-                                </ListItemText>
-                            </ListItem>
-                        </Grid>
-                    }
                     {
                         stats?.map((stat, i) => {
                             return(
@@ -42,28 +30,38 @@ export const StatsModal = ({ open, onClose, stats }) => {
                             );
                         })
                     }
-                    <Typography variant="h6">The Most Abundant Resource:</Typography>
-                    <Grid item className={styles["modal__container__stats"]}>
-                        <ListItem>
-                            <ListItemAvatar>
-                                { maResource !== '--' ? <Avatar src={`/${maResource}_icon.jpg`} /> : <Avatar><InsertPhotoOutlinedIcon /></Avatar> }
-                            </ListItemAvatar>
-                            <ListItemText>
-                                <Typography variant="h6">{maResource}</Typography>
-                            </ListItemText>
-                        </ListItem>
-                    </Grid>
-                    <Typography variant="h6">The Least Abundant Resource:</Typography>
-                    <Grid item className={styles["modal__container__stats"]}>
-                        <ListItem>
-                            <ListItemAvatar>
-                                { laResource !== '--' ? <Avatar src={`/${laResource}_icon.jpg`} /> : <Avatar><InsertPhotoOutlinedIcon /></Avatar> }
-                            </ListItemAvatar>
-                            <ListItemText>
-                                <Typography variant="h6">{laResource}</Typography>
-                            </ListItemText>
-                        </ListItem>
-                    </Grid>
+                    {
+                        maResource &&
+                        <>
+                            <Typography variant="h6">The Most Abundant Resource:</Typography>
+                            <Grid item className={styles["modal__container__stats"]}>
+                                <ListItem>
+                                    <ListItemAvatar>
+                                        { maResource !== '--' ? <Avatar src={`/${maResource}_icon.jpg`} /> : <Avatar><InsertPhotoOutlinedIcon /></Avatar> }
+                                    </ListItemAvatar>
+                                    <ListItemText>
+                                        <Typography variant="h6">{maResource}</Typography>
+                                    </ListItemText>
+                                </ListItem>
+                            </Grid>
+                        </>
+                    }
+                    {
+                        laResource &&
+                        <>
+                            <Typography variant="h6">The Least Abundant Resource:</Typography>
+                            <Grid item className={styles["modal__container__stats"]}>
+                                <ListItem>
+                                    <ListItemAvatar>
+                                        { laResource !== '--' ? <Avatar src={`/${laResource}_icon.jpg`} /> : <Avatar><InsertPhotoOutlinedIcon /></Avatar> }
+                                    </ListItemAvatar>
+                                    <ListItemText>
+                                        <Typography variant="h6">{laResource}</Typography>
+                                    </ListItemText>
+                                </ListItem>
+                            </Grid>
+                        </>
+                    }
                     </Grid>
                 </Grid>
             </Box>
