9eff9a29f0382248d12338988b29a6c0f8fa95e7
TXR3WP9 <trevor_richardson@homedepot.com>
2022-04-26 00:32:33 -0400

    updates to button sizing

--- components/game-board/game-board.js
+++ components/game-board/game-board.js
@@ -1,12 +1,11 @@
 import { HexRow } from '../hex-row/hex-row';
-import { Grid } from "@mui/material";
-import { useEffect, useRef, useState } from "react";
-import { ButtonRow } from "../button-row/button-row";
+import { Grid } from '@mui/material';
+import { useEffect, useRef, useState } from 'react';
+import { ButtonRow } from '../button-row/button-row';
 import { useCatanLogic } from '../../hooks/useCatanLogic';
-import styles from './game-board.module.scss';
-import Head from "next/head";
-import { Header } from "../header/header";
-import { useRouter } from "next/router";
+import Head from 'next/head';
+import { Header } from '../header/header';
+import { useRouter } from 'next/router';
 
 export const GameBoard = ({ props }) => {
 
@@ -27,6 +26,7 @@ export const GameBoard = ({ props }) => {
 
     const { boardData, stats, generateBoardData } = useCatanLogic(numbers_freq, resources_freq, row_config, port_config, ports);
 
+    const titleRef = useRef();
     const gameBoardRef = useRef();
     const heightRef = useRef();
 
@@ -34,40 +34,51 @@ export const GameBoard = ({ props }) => {
     const [availableHeight, setAvailableHeight] = useState(typeof window !== "undefined" ? window.innerHeight : 0);
     const [screenWidth, setScreenWidth] = useState(typeof window !== "undefined" ? screen.width : 0);
     const [scale, setScale] = useState(1);
+    const [topMargin, setTopMargin] = useState(0);
     const [transformOrigin, setTransformOrigin] = useState(null);
 
     useEffect(() => {
         const rowWidth = gameBoardRef.current.clientWidth;
         const boardHeight = heightRef.current.clientHeight;
+        const totalHeight = boardHeight + titleRef.current.clientHeight + 161;
         if (availableWidth <= rowWidth) {
             setScale(screenWidth / rowWidth);
             setTransformOrigin('top left');
-        } else if (boardHeight > availableHeight) {
-            setScale(availableHeight / boardHeight);
+            setTopMargin((boardHeight * (screenWidth / rowWidth)) + titleRef.current.clientHeight + 30);
+        } else if (totalHeight > availableHeight) {
+            setScale(availableHeight / totalHeight);
             setTransformOrigin('top center');
+            setTopMargin((boardHeight * (availableHeight / totalHeight)) + titleRef.current.clientHeight + 30);
         } else {
             setScale(1);
+            setTopMargin(boardHeight + titleRef.current.clientHeight + 30);
         }
     }, [boardData]);
 
     return(
-        <Grid container direction="column" ref={heightRef} className={styles["game-board"]} sx={{ transform: `scale(${scale})`, transformOrigin: `${transformOrigin}` }}>
-            <Head>
-                <title>{title} | Catan Board Generator</title>
-                <link rel="icon" href="/catan-icon.ico"/>
-            </Head>
-            <Grid item>
-                <Header title={title} />
-            </Grid>
-            <Grid item ref={gameBoardRef}>
-                {boardData?.map((row, index) => {
-                    return (
+      <>
+          <Head>
+              <title>{title} | Catan Board Generator</title>
+              <link rel="icon" href="/catan-icon.ico"/>
+          </Head>
+          <Grid container direction="column" alignItems="center" ref={titleRef}>
+              <Grid item >
+                  <Header title={title} />
+              </Grid>
+          </Grid>
+          <Grid container direction="column" ref={heightRef} alignItems="center" sx={{ transform: `scale(${scale})`, transformOrigin: `${transformOrigin}` }}>
+              <Grid item ref={gameBoardRef}>
+                  {boardData?.map((row, index) => {
+                      return (
                         <HexRow row={row.row} key={index} />
-                    )
-                })}
-                <ButtonRow generate={() => generateBoardData()} stats={stats} />
-            </Grid>
-        </Grid>
+                      )
+                  })}
+              </Grid>
+          </Grid>
+          <Grid container direction="column" alignItems="center">
+              <ButtonRow generate={() => generateBoardData()} stats={stats} top={topMargin} />
+          </Grid>
+      </>
     );
 }
 
--- pages/scenario/demo.js
+++ pages/scenario/demo.js
@@ -9,7 +9,10 @@ export const Demo = () => {
 
     const rowRef = useRef();
     const heightRef = useRef();
+    const titleRef = useRef();
+    const buttonRef = useRef();
     const [scale, setScale] = useState(0);
+    const [topMargin, setTopMargin] = useState(0);
     const [transformOrigin, setTransformOrigin] = useState(null);
     const title = 'The Settlers of Catan Demo';
 
@@ -27,20 +30,33 @@ export const Demo = () => {
         }
         const rowWidth = rowRef.current.clientWidth;
         const rowHeight = heightRef.current.clientHeight;
+        const buttonHeight = buttonRef.current.clientHeight;
+        const totalHeight = rowHeight + titleRef.current.clientHeight + 26 + 35 + 100;
         console.log(`ROW WIDTH: ${rowWidth}`);
         console.log(`ROW HEIGHT: ${rowHeight}`);
+        console.log('TITLE HEIGHT: ' + titleRef.current.clientHeight);
+        console.log(`BUTTON HEIGHT: ${buttonHeight}`);
+        console.log(`TOTAL HEIGHT: ${totalHeight}`);
         if (availableWidth <= rowWidth) {
             console.log(`SCALE WIDTH TRUE`);
             const wScale = screenWidth / rowWidth;
             setScale(wScale);
             setTransformOrigin('top left');
             console.log(`W SCALE: ${wScale}`);
-        } else if (rowHeight > availableHeight) {
+            const topMargin = (rowHeight * wScale) + titleRef.current.clientHeight + 30;
+            console.log('TOP MARGIN: ' + topMargin);
+            setTopMargin(topMargin);
+        } else if (totalHeight > availableHeight) {
             console.log(`SCALE HEIGHT TRUE`);
-            const hScale = availableHeight / rowHeight;
+            const hScale = availableHeight / totalHeight;
             setScale(hScale);
             setTransformOrigin('top center');
             console.log(`H SCALE: ${hScale}`);
+            const boardHeight = rowHeight * hScale;
+            console.log('BOARD HEIGHT: ' + boardHeight);
+            const topMargin = (rowHeight * hScale) + titleRef.current.clientHeight + 30;
+            console.log('TOP MARGIN: ' + topMargin);
+            setTopMargin(topMargin);
         } else {
             console.log(`SCALE FALSE - setting scale to 1`);
             setScale(1);
@@ -50,31 +66,37 @@ export const Demo = () => {
     const hex = { resource: 'Ore', token: { number: '8', probability: '\u2022\u2022\u2022\u2022\u2022'}};
 
     return(
-        <Grid container direction="column" ref={heightRef} alignItems="center" sx={{ transform: `scale(${scale})`, transformOrigin: `${transformOrigin}`}}>
-            <Head>
-                <title>{title} | Catan Board Generator</title>
-                <link rel="icon" href="/catan-icon.ico"/>
-            </Head>
-            <Grid item>
-                <Header title={title} />
+        <>
+            <Grid container direction="column" alignItems="center" ref={titleRef}>
+                <Head>
+                    <title>{title} | Catan Board Generator</title>
+                    <link rel="icon" href="/catan-icon.ico"/>
+                </Head>
+                <Grid item >
+                    <Header title={title} />
+                </Grid>
             </Grid>
-            <Grid item ref={rowRef}>
-                {
-                    row_config.map((value, index) => {
-                        return(
-                            <Grid container direction="row" wrap="nowrap" justifyContent="center" mb="-26px" key={index}>
-                                {
-                                    Array.from(Array(value), (e, i) => {
-                                        return(<Hex hex={hex} key={i} />);
-                                    })
-                                }
-                            </Grid>
-                        );
-                    })
-                }
-                <ButtonRow />
+            <Grid container direction="column" alignItems="center" ref={heightRef} sx={{ transform: `scale(${scale})`, transformOrigin: `${transformOrigin}`}}>
+                <Grid item ref={rowRef}>
+                    {
+                        row_config.map((value, index) => {
+                            return(
+                              <Grid container direction="row" wrap="nowrap" justifyContent="center" mb="-26px" key={index}>
+                                  {
+                                      Array.from(Array(value), (e, i) => {
+                                          return(<Hex hex={hex} key={i} />);
+                                      })
+                                  }
+                              </Grid>
+                            );
+                        })
+                    }
+                </Grid>
             </Grid>
-        </Grid>
+            <Grid container direction="column" alignItems="center" ref={buttonRef}>
+                <ButtonRow top={topMargin}/>
+            </Grid>
+    </>
     );
 }
 
