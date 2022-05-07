1246219224b783f93d8494217b5f818ffc6ef012
TXR3WP9 <trevor_richardson@homedepot.com>
2022-05-07 00:55:17 -0400

    integrating contentful for game descriptions

--- hooks/useGameDescriptions.js
+++ hooks/useGameDescriptions.js
@@ -1,20 +1,37 @@
+import { useEffect, useState } from 'react';
+
+const contentful = require('contentful');
+
 export const useGameDescriptions = () => {
-  const descriptions = [
-    {
-      title: "The Settlers of Catan",
-      subtitle: "The game that started it all! Generate a random board setup for the original island of Catan",
-      players: "3 - 4",
-      imageUrl: "select-catan.png",
-      pageUrl: "catan"
-    },
-    {
-      title: "5 & 6 Player Extension",
-      subtitle: "Generate a random board setup for the original island of Catan expanded for 5 or 6 players",
-      players: "5 - 6",
-      imageUrl: "select-catan-5_6.png",
-      pageUrl: "catan5_6ext"
-    }
-  ];
+
+  const [descriptions, setDescriptions] = useState(null);
+
+  let client = contentful.createClient({
+    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
+  });
+
+  const getScenarioDescription = async() => {
+    let results = await client.getEntries({
+      content_type: 'scenarioDescription'
+    });
+    const { items } = results;
+    return (items || []).map(item => item.fields).sort((a, b) => a.order - b.order);
+  }
+
+  useEffect(() => {
+    getScenarioDescription().then((data) => {
+      const desc = data?.map(description => {
+        const { image } = description;
+        const imageUrl = `https:${image?.fields?.file?.url}`;
+        return {
+          ...description,
+          imageUrl: imageUrl
+        }
+      });
+      setDescriptions(desc);
+    }).catch(() => {});
+  }, []);
 
   return { descriptions };
 }
