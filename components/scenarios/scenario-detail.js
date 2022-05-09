bbbfeb3a9adeaf52ab4a75fc730426f3a00bb0e3
TXR3WP9 <trevor_richardson@homedepot.com>
2022-05-08 23:54:16 -0400

    isLoading updates

--- components/scenarios/scenario-detail.js
+++ components/scenarios/scenario-detail.js
@@ -1,10 +1,10 @@
-import { ButtonBase, Divider, Grid, Modal, Skeleton, Typography } from '@mui/material';
+import { ButtonBase, Divider, Grid, Modal, Typography } from '@mui/material';
 import styles from './scenarios.module.scss';
 import { useEffect, useState } from 'react';
 import { useGameContext } from '../game-select/game-context';
 import { HarborModal } from './harbor-modal';
 
-export const ScenarioDetail = ({ description, isLoading }) => {
+export const ScenarioDetail = ({ description }) => {
 
   const { title, subtitle, imageUrl, pageUrl } = description;
   const [open, setOpen] = useState(false);
@@ -22,19 +22,19 @@ export const ScenarioDetail = ({ description, isLoading }) => {
 
   return(
     <>
-    <ButtonBase sx={{ height: '100%' }} onClick={handleButtonClick} disabled={isLoading}>
+    <ButtonBase sx={{ height: '100%' }} onClick={handleButtonClick}>
       <Grid container direction="column" className={styles["detail"]}>
         <Grid item>
-          { isLoading ? <Skeleton animation="wave" height={100} width="100%" /> : <img className={styles["detail__image"]} src={imageUrl} alt={imageUrl}/> }
+          <img className={styles["detail__image"]} src={imageUrl} alt={imageUrl}/>
         </Grid>
         <Grid item pt={1}>
-          <Typography variant="h6" lineHeight="1">{ isLoading ? <Skeleton animation="wave" /> : title }</Typography>
+          <Typography variant="h6" lineHeight="1">{title}</Typography>
         </Grid>
         <Grid item pt={1}>
           <Divider variant="middle" />
         </Grid>
         <Grid item p={1}>
-          <Typography variant="body2" lineHeight="1" textAlign="left" color="secondary">{ isLoading ? <Skeleton animation="wave" /> : subtitle }</Typography>
+          <Typography variant="body2" lineHeight="1" textAlign="left" color="secondary">{subtitle}</Typography>
         </Grid>
         {/*<Grid item>*/}
         {/*  <PersonIcon sx={{ verticalAlign: 'middle', fontSize: '0.75rem' }}/><Typography variant="caption">&nbsp;{players} players</Typography>*/}
--- components/scenarios/scenarios.js
+++ components/scenarios/scenarios.js
@@ -1,4 +1,4 @@
-import { Grid } from '@mui/material';
+import { Divider, Grid, Skeleton } from '@mui/material';
 import styles from './scenarios.module.scss';
 import { ScenarioDetail } from './scenario-detail';
 import { useGameDescriptions } from '../../hooks/useGameDescriptions';
@@ -7,13 +7,36 @@ export const Scenarios = () => {
 
   const { descriptions, isLoading } = useGameDescriptions();
 
+  if (isLoading) {
+    return(
+      <Grid container direction="row" className={styles["scenarios"]} spacing={2}>
+        <Grid item xs={6} md={4}>
+          <Grid container direction="column" className={styles["detail"]}>
+            <Grid item>
+              <Skeleton animation="wave" height={150} />
+            </Grid>
+            <Grid item pt={1}>
+              <Skeleton animation="wave" />
+            </Grid>
+            <Grid item pt={1}>
+              <Divider variant="middle" />
+            </Grid>
+            <Grid item p={1}>
+              <Skeleton animation="wave" />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
+  }
+
   return(
     <Grid container direction="row" className={styles["scenarios"]} spacing={2}>
       {
         descriptions?.map((description, i) => {
           return(
             <Grid item xs={6} md={4} key={i}>
-              <ScenarioDetail description={description} isLoading={isLoading} />
+              <ScenarioDetail description={description} />
             </Grid>
           );
         })
