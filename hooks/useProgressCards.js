d0427922b8c3ffba9948dccc8b6d0d4684da8346
TXR3WP9 <trevor_richardson@homedepot.com>
2022-06-20 01:01:30 -0400

    adding implementation for progress cards

---
+++ hooks/useProgressCards.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import { useContentfulContext } from '../contexts/contentful-context';
+
+export const useProgressCards = () => {
+
+  const [progressCards, setProgressCards] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const { client } = useContentfulContext();
+
+  const getProgressCards = async () => {
+    let results = await client.getEntries({
+      content_type: 'progressCards',
+    });
+    const {items} = results;
+    return (items || []).map(item => item.fields);
+  }
+
+  useEffect(() => {
+    getProgressCards().then((data) => {
+      const desc = data?.map(content => {
+        return {
+          name: content?.name,
+          category: content?.category,
+          image: content?.image?.fields?.file?.url,
+          description: content?.description,
+          cardColor: content?.cardColor,
+          noOfCards: content?.noOfCards
+        }
+      });
+      setProgressCards(desc);
+      setIsLoading(false);
+    }).catch(() => {
+    });
+  }, []);
+
+  return { progressCards, isLoading };
+}
