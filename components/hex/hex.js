1b30c020b267ab42619a465b1fc3367473dd869d
TXR3WP9 <trevor_richardson@homedepot.com>
2022-06-24 14:27:18 -0400

    moving remaining images to contentful

--- components/hex/hex.js
+++ components/hex/hex.js
@@ -1,32 +1,51 @@
 import { Box, ButtonBase } from '@mui/material';
 import styles from './hex.module.scss';
 import { Token } from '../token/token';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { HexModal } from '../hex-modal/hex-modal';
+import { useResources } from '../../hooks/useResources';
+import Image from 'next/image';
+import { useHarbors } from '../../hooks/useHarbors';
 
 export const Hex = ({ hex }) => {
 
-    const [open, setOpen] = useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-    let imageUrl = 'url(../sea_hex.png)';
+  const { resources } = useResources();
+  const { harbors } = useHarbors();
+  const [ open, setOpen ] = useState(false);
+  const [ backgroundImage, setBackgroundImage ] = useState(undefined);
 
-    if (hex?.resource?.includes('3for1') || hex?.resource?.includes('2for1')) {
-        imageUrl = `url(../${hex?.resource}/${hex?.resource}_${hex?.rotation}.png)`;
-    } else if (hex?.resource !== '') {
-        imageUrl = `url(../${hex?.resource}/hex.png)`;
+  useEffect(() => {
+      if (hex?.terrain !== "Harbor" && hex?.terrain !== "") {
+        const resource = resources?.find(r => r?.resource === hex?.resource);
+        setBackgroundImage(resource?.hexImage);
+      } else if (hex?.terrain === "Harbor") {
+        const harbor = harbors?.find(h => h?.id === hex?.resource);
+        const image = harbor?.hexImage?.find(i => i[hex?.rotation]);
+        setBackgroundImage(image?.[hex?.rotation]);
+      } else {
+        const sea = resources?.find(r => r?.resource === "Sea");
+        setBackgroundImage(sea?.hexImage);
+      }
+    return () => {
+      setBackgroundImage(null);
     }
+  });
 
-    return(
-        <>
-        <ButtonBase className={styles["hex__button"]} onClick={handleOpen}>
-            <Box className={styles["hex"]} sx={{ backgroundImage: imageUrl }}>
-                <Token number={hex?.token?.number} probability={hex?.token?.probability} />
-            </Box>
-        </ButtonBase>
-        <HexModal open={open} onClose={handleClose} hex={hex} />
-        </>
-    );
+  const imageLoader = ({ src }) => {
+    return `${src}?w=500&h=575&fm=webp`;
+  }
+
+  return (
+    <>
+      <ButtonBase className={styles["hex__button"]} onClick={() => setOpen(true)}>
+        <Box className={styles["hex"]} position="relative">
+          <Image loader={imageLoader} src={`https:${backgroundImage}`} layout="fill" priority={true} />
+          <Token number={hex?.token?.number} probability={hex?.token?.probability}/>
+        </Box>
+      </ButtonBase>
+      <HexModal open={open} onClose={() => setOpen(false)} hex={hex}/>
+    </>
+  );
 };
 
 Hex.displayName = 'Hex';
